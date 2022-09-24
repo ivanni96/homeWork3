ce36537aded2020f0dd3324732c2e46833d48a6e
ivan <ivanni95@gmail.com>
2022-09-24 08:45:21 +0400

    решил задачу 1

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        byte otradny = 50;
+        System.out.println(otradny);
+        short samara = 2000;
+        System.out.println(samara);
+        int ufa = 1500;
+        System.out.println(ufa);
+        long moscow = 28000;
+        System.out.println(moscow);
+        float buzuluk = 2000/20;
+        System.out.println(buzuluk);
+        double chelyabinsk = 1500.1234;
+        System.out.println(chelyabinsk);
+    }}
